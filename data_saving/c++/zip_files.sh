593d69b26d8905e3a35c42c945b7eb5de206e287
EaswarGn <easwar.chess@gmail.com>
2025-02-18 17:42:49 -0800

    removed old code

--- data_saving/c++/zip_files.sh
+++ data_saving/c++/zip_files.sh
@@ -1,12 +1,12 @@
 
-cd /Volumes/Lexar/chessmodel_dataset/ranged_testing_chunks
+cd /Volumes/Lexar/1900_training_chunks
 
 for folder in */; do
   # Check if it's a directory
   if [ -d "$folder" ]; then
     # Remove trailing slash and create zip file
     folder_name=$(basename "$folder")
-    zip -r "/Volumes/Lexar/zipped_testing/${folder_name}.zip" "$folder_name"
+    zip -r "/Volumes/Lexar/1900_training_zipped/1900_zipped_training_chunks/${folder_name}.zip" "$folder_name"
     echo "Zipped $folder_name"
   fi
 done
