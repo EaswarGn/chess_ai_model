77fed8509e953901f88eb6a6be455065c323886f
EaswarGn <easwar.chess@gmail.com>
2025-02-24 18:51:45 -0800

    retesting ablation 1

--- training/ddp_training/train_ddp.py
+++ training/ddp_training/train_ddp.py
@@ -112,6 +112,7 @@ def train_model_ddp(rank, world_size, CONFIG):
         for key, value in state_dict.items():
             new_key = key.replace('_orig_mod.', '')
             #new_key = new_key.replace('module.', '')
+            new_key = 'module.'+new_key
             new_state_dict[new_key] = value
         model.load_state_dict(new_state_dict, strict=True)
         optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
