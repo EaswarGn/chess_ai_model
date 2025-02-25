fixed bug in loss computation

training/ddp_training/configs/models/ddp_config.py
@@ -13,7 +13,7 @@ from .utils.time_controls import time_controls_encoded
 ###############################
 
 NAME = "ddp_config"  # name and identifier for this configuration
-GPU_ID = [0, 1]#, 2, 3, 4, 5, 6, 7]
+NUM_GPUS = 2#, 2, 3, 4, 5, 6, 7]
 
 ###############################
 ######### Dataloading #########
@@ -21,7 +21,7 @@ GPU_ID = [0, 1]#, 2, 3, 4, 5, 6, 7]
 
 #DATASET = ChessDatasetFT  # custom PyTorch dataset
 BATCH_SIZE = 512  # batch size
-NUM_WORKERS = mp.cpu_count()//len(GPU_ID)  # number of workers to use for dataloading
+NUM_WORKERS = mp.cpu_count()//NUM_GPUS  # number of workers to use for dataloading
 PREFETCH_FACTOR = 2  # number of batches to prefetch per worker
 PIN_MEMORY = False  # pin to GPU memory when dataloading?
 
@@ -80,22 +80,40 @@ EPSILON = 1e-9  # epsilon term in the Adam optimizer
 LABEL_SMOOTHING = 0.1  # label smoothing co-efficient in the Cross Entropy loss
 BOARD_STATUS_LENGTH = 70  # total length of input sequence
 USE_AMP = True  # use automatic mixed precision training?
+OPTIMIZER = torch.optim.Adam  # optimizer
+CHECKPOINT_PATH = None
+
+
+###############################
+########### Outputs ##########
+###############################
+move_time_head = nn.Sequential(
+            nn.Linear(D_MODEL, 1),
+        )
+game_length_head = nn.Sequential(
+            nn.Linear(D_MODEL, 1),
+        )
+categorical_game_result_head = nn.Sequential(
+            nn.Linear(D_MODEL, 3),
+            nn.Softmax(dim=-1)  # Changed to Softmax to output probabilities
+        )
+game_result_head = None
+
+
+###############################
+########### LOSS ##########
+###############################
 CRITERION = LabelSmoothedCE  # training criterion (loss)
 LOSS_WEIGHTS = {
     'move_loss_weight': 1.0,
-    'move_time_loss_weight': 1.0,
-    'game_result_loss_weight': 1.0,
+    'move_time_loss_weight': 0.5,
+    'game_result_loss_weight': 0.0,
     'moves_until_end_loss_weight': 1.0,
     'categorical_game_result_loss_weight': 1.0
 }
-LOSSES = {
-    'move_loss': CRITERION(
-        eps=LABEL_SMOOTHING, n_predictions=N_MOVES
-    ),
-    'move_time_loss': nn.L1Loss(),
-    #'game_result_loss': nn.L1Loss(),
-    'moves_until_end_loss': nn.L1Loss(),
-    'categorical_game_result_loss': nn.CrossEntropyLoss()
-}
-OPTIMIZER = torch.optim.Adam  # optimizer
-CHECKPOINT_PATH = None
+
+move_loss =  CRITERION
+move_time_loss= nn.L1Loss(),
+#'game_result_loss': nn.L1Loss(),
+moves_until_end_loss= nn.L1Loss(),
+categorical_game_result_loss= nn.CrossEntropyLoss()

training/ddp_training/criteria_ddp.py
@@ -84,14 +84,13 @@ class MultiTaskChessLoss(nn.Module):
         super().__init__()
         self.device = device
         
-        # Initialize learnable weights (log-space for stability)
-        self.log_vars = nn.Parameter(torch.zeros(len(CONFIG.LOSS_WEIGHTS)), requires_grad=True)
+        self.loss_weights = CONFIG.LOSS_WEIGHTS
         
         self.loss_functions = {
-            'move_loss': LabelSmoothedCE(DEVICE=device, eps=CONFIG.LABEL_SMOOTHING, n_predictions=CONFIG.N_MOVES),
-            'time_loss': nn.L1Loss(),
-            'moves_until_end_loss': nn.L1Loss(),
-            'categorical_game_result_loss': nn.CrossEntropyLoss()
+            'move_loss': CONFIG.move_loss(DEVICE=device, eps=CONFIG.LABEL_SMOOTHING, n_predictions=CONFIG.N_MOVES),
+            'time_loss': CONFIG.move_time_loss,
+            'moves_until_end_loss': CONFIG.moves_until_end_loss,
+            'categorical_game_result_loss': CONFIG.categorical_game_result_loss
         }
 
     def forward(self, predictions, targets):
@@ -99,6 +98,11 @@ class MultiTaskChessLoss(nn.Module):
 
         for i, key in enumerate(self.loss_functions):
             loss_fn = self.loss_functions[key]
+            
+            if loss_fn is None:
+                loss = torch.tensor(0.0)
+                individual_losses[key] = loss
+                continue
             
             if key == 'move_loss':
                 loss = loss_fn(predictions['from_squares'], targets["from_squares"], targets["lengths"]) + \
@@ -130,11 +134,8 @@ class MultiTaskChessLoss(nn.Module):
             
         individual_losses['result_loss'] = torch.tensor(0.0)
 
-        # Compute dynamically weighted loss
         total_loss = 0.0
         for i, key in enumerate(individual_losses):
-            precision = torch.exp(-self.log_vars[i])
-            weighted_loss = precision * individual_losses[key] + self.log_vars[i]
-            total_loss += weighted_loss
+            total_loss += self.loss_weight[key+'_weight'] * individual_losses[key]
 
         return total_loss, individual_losses

training/ddp_training/model_ddp.py
@@ -53,16 +53,9 @@ class ChessTemporalTransformerEncoder(nn.Module):
         self.from_squares = nn.Linear(CONFIG.D_MODEL, 1)
         self.to_squares = nn.Linear(CONFIG.D_MODEL, 1)
         self.game_result_head = None
-        self.move_time_head = nn.Sequential(
-            nn.Linear(CONFIG.D_MODEL, 1),
-        )
-        self.game_length_head = nn.Sequential(
-            nn.Linear(CONFIG.D_MODEL, 1),
-        )
-        self.categorical_game_result_head = nn.Sequential(
-            nn.Linear(CONFIG.D_MODEL, 3),
-            nn.Softmax(dim=-1)  # Changed to Softmax to output probabilities
-        )
+        self.move_time_head = CONFIG.move_time_head
+        self.game_length_head = CONFIG.game_length_head
+        self.categorical_game_result_head = CONFIG.categorical_game_result_head
         
         # Create task-specific CLS tokens
         self.moves_remaining_cls_token = nn.Parameter(torch.randn(1, 1, self.d_model))

training/ddp_training/train_ddp.py
@@ -72,7 +72,7 @@ def train_model_ddp(rank, world_size, CONFIG):
     
     os.environ['TORCH_DISTRIBUTED_DEBUG'] = 'INFO' 
     
-    DEVICE = torch.device(f"cuda:{CONFIG.GPU_ID[rank]}")
+    DEVICE = torch.device(f"cuda:{rank}")
     if rank == 0:
         print(f"Training on {world_size} GPUs")
         os.makedirs(f"{CONFIG.NAME}/logs/main_log", exist_ok=True)
@@ -85,7 +85,7 @@ def train_model_ddp(rank, world_size, CONFIG):
     # Model
     model = ChessTemporalTransformerEncoder(CONFIG, DEVICE=DEVICE).to(DEVICE)
 
-    model = DDP(model, device_ids=[CONFIG.GPU_ID[rank]], find_unused_parameters=True)
+    model = DDP(model, device_ids=[rank], find_unused_parameters=True)
 
     # Optimizer
     optimizer = torch.optim.Adam(
@@ -562,10 +562,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     CONFIG = import_config(args.config_name)
     
-    from picklableconfig import convert_config_to_picklable
-    CONFIG = convert_config_to_picklable(CONFIG)
+    #from picklableconfig import convert_config_to_picklable
+    #CONFIG = convert_config_to_picklable(CONFIG)
     
-    world_size = len(CONFIG.GPU_ID)
+    world_size = CONFIG.NUM_GPUS
     mp.spawn(
         train_model_ddp,
         args=(world_size, CONFIG),