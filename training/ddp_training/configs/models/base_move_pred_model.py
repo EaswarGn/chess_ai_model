9427b401d774508c16118296db082db442f87663
EaswarGn <easwar.chess@gmail.com>
2025-02-26 13:37:04 -0800

    created config for base move pred model for MTL learning

--- training/ddp_training/configs/models/base_move_pred_model.py
+++ training/ddp_training/configs/models/base_move_pred_model.py
@@ -15,7 +15,7 @@ class CONFIG:
         ############ Name #############
         ###############################
         self.NAME = "ddp_config"
-        self.NUM_GPUS = 1
+        self.NUM_GPUS = torch.cuda.device_count()
 
         ###############################
         ######### Dataloading #########
