b873e6c127833d879fa2eec7c8be7724bc01ebae
EaswarGn <easwar.chess@gmail.com>
2025-02-25 21:01:30 -0800

    created ddp config files

--- training/ddp_training/configs/models/ddp_config.py
+++ training/ddp_training/configs/models/ddp_config.py
@@ -95,9 +95,9 @@ class CONFIG:
         self.CRITERION = LabelSmoothedCE
         self.LOSS_WEIGHTS = {
             "move_loss_weight": 1.0,
-            "time_loss_weight": 0.5,
+            "time_loss_weight": 0.25,
             "result_loss_weight": 0.0,
-            "moves_until_end_loss_weight": 0.25,
+            "moves_until_end_loss_weight": 0.125,
             "categorical_game_result_loss_weight": 1.0,
         }
 
