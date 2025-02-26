2a163b96bc4340533ba5a25196ed84e2868a3c15
EaswarGn <easwar.chess@gmail.com>
2025-02-26 13:36:23 -0800

    created config for base move pred model for MTL learning

--- training/ddp_training/configs/models/base_move_pred_model.py
+++ training/ddp_training/configs/models/base_move_pred_model.py
@@ -15,7 +15,7 @@ class CONFIG:
         ############ Name #############
         ###############################
         self.NAME = "ddp_config"
-        self.NUM_GPUS = 2
+        self.NUM_GPUS = 1
 
         ###############################
         ######### Dataloading #########
