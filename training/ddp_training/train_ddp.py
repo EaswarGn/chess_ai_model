de9376a5d4c1c9db483d9597b7424fbc24725535
EaswarGn <easwar.chess@gmail.com>
2025-02-26 17:47:32 -0800

    testing with pretrained weights

--- training/ddp_training/train_ddp.py
+++ training/ddp_training/train_ddp.py
@@ -115,10 +115,10 @@ def train_model_ddp(rank, world_size, CONFIG):
             new_key = 'module.'+new_key
             new_state_dict[new_key] = value
         model.load_state_dict(new_state_dict, strict=CONFIG.USE_STRICT)
+        print(checkpoint["optimizer_state_dict"])
         optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-        
-        if rank == 0:
-            print(f"\nLoaded checkpoint from epoch {start_epoch}.\n")
+
+        print(f"\nLoaded checkpoint from step {step}.\n")
 
     # Compile model
     compiled_model = torch.compile(
