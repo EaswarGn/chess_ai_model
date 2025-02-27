ac41eb265add001f693acac63a227361d4ca7e9d
EaswarGn <easwar.chess@gmail.com>
2025-02-26 21:59:16 -0800

    testing validation with ddp

--- training/ddp_training/train_ddp.py
+++ training/ddp_training/train_ddp.py
@@ -99,8 +99,48 @@ def train_model_ddp(rank, world_size, CONFIG):
     steps_per_epoch = CONFIG.STEPS_PER_EPOCH
     epochs = total_steps//steps_per_epoch
 
+    
+
+    criterion = LabelSmoothedCE(DEVICE=DEVICE, eps=CONFIG.LABEL_SMOOTHING, n_predictions=CONFIG.N_MOVES).to(DEVICE)
+    scaler = GradScaler(enabled=CONFIG.USE_AMP)
+    
+    training_file_list = get_all_record_files('../../../1900_zipped_training_chunks')
+    training_file_list = [file for file in training_file_list if file.endswith('.zst')]   
+    training_file_list = [s for s in training_file_list if "._" not in s]
+    
+    rand_folder = random.randint(1, 3)
+    testing_file_list = get_all_record_files(f'../../../ranged_chunks_zipped/1900/{rand_folder}_chunks')
+    testing_file_list = [file for file in testing_file_list if file.endswith('.zst')]
+    testing_file_list = [s for s in testing_file_list if "._" not in s]
+    testing_file_list = random.sample(testing_file_list, min(2, len(testing_file_list)))
+    
+    train_dataset = ChunkLoader(training_file_list, record_dtype, rank, world_size)
+    val_dataset = ChunkLoader(testing_file_list, record_dtype, rank, world_size)
+
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=CONFIG.BATCH_SIZE // world_size,
+        num_workers=CONFIG.NUM_WORKERS,
+        pin_memory=CONFIG.PIN_MEMORY,
+        prefetch_factor=CONFIG.PREFETCH_FACTOR,
+    )
+
+    val_loader = DataLoader(
+        dataset=val_dataset,
+        batch_size=CONFIG.BATCH_SIZE // world_size,
+        num_workers=CONFIG.NUM_WORKERS,
+        pin_memory=CONFIG.PIN_MEMORY,
+        prefetch_factor=CONFIG.PREFETCH_FACTOR,
+    )
+    
+    for i, batch in enumerate(train_loader):
+        for key in batch:
+            batch[key] = batch[key].to(DEVICE)
+        predictions = model(batch)
+        break
+
     if CONFIG.CHECKPOINT_PATH is not None and rank == 0:
-        checkpoint = torch.load(CONFIG.CHECKPOINT_PATH, map_location=DEVICE, weights_only=True,)
+        checkpoint = torch.load(CONFIG.CHECKPOINT_PATH, map_location=DEVICE)
         step = checkpoint['step']
         step = int(step)
         start_epoch = step//CONFIG.STEPS_PER_EPOCH + 1
@@ -144,38 +184,6 @@ def train_model_ddp(rank, world_size, CONFIG):
 
         
     model = DDP(compiled_model, device_ids=[rank], find_unused_parameters=True)
-
-    criterion = LabelSmoothedCE(DEVICE=DEVICE, eps=CONFIG.LABEL_SMOOTHING, n_predictions=CONFIG.N_MOVES).to(DEVICE)
-    scaler = GradScaler(enabled=CONFIG.USE_AMP)
-    
-    training_file_list = get_all_record_files('../../../1900_zipped_training_chunks')
-    training_file_list = [file for file in training_file_list if file.endswith('.zst')]   
-    training_file_list = [s for s in training_file_list if "._" not in s]
-    
-    rand_folder = random.randint(1, 3)
-    testing_file_list = get_all_record_files(f'../../../ranged_chunks_zipped/1900/{rand_folder}_chunks')
-    testing_file_list = [file for file in testing_file_list if file.endswith('.zst')]
-    testing_file_list = [s for s in testing_file_list if "._" not in s]
-    testing_file_list = random.sample(testing_file_list, min(2, len(testing_file_list)))
-    
-    train_dataset = ChunkLoader(training_file_list, record_dtype, rank, world_size)
-    val_dataset = ChunkLoader(testing_file_list, record_dtype, rank, world_size)
-
-    train_loader = DataLoader(
-        dataset=train_dataset,
-        batch_size=CONFIG.BATCH_SIZE // world_size,
-        num_workers=CONFIG.NUM_WORKERS,
-        pin_memory=CONFIG.PIN_MEMORY,
-        prefetch_factor=CONFIG.PREFETCH_FACTOR,
-    )
-
-    val_loader = DataLoader(
-        dataset=val_dataset,
-        batch_size=CONFIG.BATCH_SIZE // world_size,
-        num_workers=CONFIG.NUM_WORKERS,
-        pin_memory=CONFIG.PIN_MEMORY,
-        prefetch_factor=CONFIG.PREFETCH_FACTOR,
-    )
 
     train_epoch(
         rank=rank,
