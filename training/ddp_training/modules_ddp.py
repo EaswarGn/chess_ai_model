876873b2b15819c56bb8e9bf0c8e722fd84d60a8
EaswarGn <easwar.chess@gmail.com>
2025-03-03 21:12:39 -0800

    updated dataset compatibility with original code

--- training/ddp_training/modules_ddp.py
+++ training/ddp_training/modules_ddp.py
@@ -556,9 +556,9 @@ class BoardEncoder(nn.Module):
             torch.FloatTensor: Encoded board representation
         """
         batch_size = turns.size(0)
-        print(time_control.unsqueeze(-1).to(torch.float32).shape)
-        print(self.time_control_projection(time_control).shape)
-        print(self.move_number_projection(move_number.unsqueeze(-1).to(torch.float32)).shape)
+        print("1:",time_control.unsqueeze(-1).to(torch.float32).shape)
+        print("2:",self.time_control_projection(time_control).shape)
+        print("3:",self.move_number_projection(move_number.unsqueeze(-1).to(torch.float32)).shape)
         
         sys.exit()
 
