fec65759c76272a6cc1f740cdb5e3efe0a11e848
EaswarGn <easwar.chess@gmail.com>
2025-03-08 20:22:38 -0800

    experimenting new prediction head

--- training/ddp_training/model_ddp.py
+++ training/ddp_training/model_ddp.py
@@ -148,8 +148,8 @@ class ExperimentalTransformer(nn.Module):
             # Update boards: set the 'from square' positions to all ones (or any special value you choose)
             for i in range(batch_size):
                 from_idx = from_square_idx[i].item()  # Get the index of the from square for this example
-                print(from_idx)
-                boards[i, 14+self.num_cls_tokens+from_idx, :] = 1.0  # Set all channels at this index to 1
+                #print(from_idx)
+                boards[i, 14+self.num_cls_tokens+from_idx, :] = 100.0  # Set all channels at this index to 1
         
         # Predict to squares (use the updated boards)
         to_squares = (self.to_squares(boards[:, 14+self.num_cls_tokens:, :]).squeeze(2).unsqueeze(1)) if self.to_squares is not None else None
