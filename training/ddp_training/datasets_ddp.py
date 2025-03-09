ee217a250618c3223e8be8d654acc23d40531c88
EaswarGn <easwar.chess@gmail.com>
2025-03-09 11:22:25 -0700

    experimenting new prediction head

--- training/ddp_training/datasets_ddp.py
+++ training/ddp_training/datasets_ddp.py
@@ -78,7 +78,8 @@ class ChunkLoader(IterableDataset):
         self.stats = None
         with open('stats_dict.pkl', 'rb') as file:
             self.stats = pickle.load(file)
-        
+        self.stats['move_number']['mean'] = self.stats['move_number']['mean']/2.0
+        self.stats['move_number']['std'] = self.stats['move_number']['std']/2.0
 
     def get_chunk_size(self):
         with open(self.file_list[0], "rb") as f:
@@ -194,10 +195,6 @@ class ChunkLoader(IterableDataset):
                     
                     #normalize continuous variables through z-scores:
                     record['move_number'] = record['move_number']//2
-                    print(self.stats['move_number']['mean'])
-                    print(self.stats['move_number']['std'])
-                    self.stats['move_number']['mean'] = self.stats['move_number']['mean']/2.0
-                    self.stats['move_number']['std'] = self.stats['move_number']['std']/2.0
                     
                     for key in self.stats:
                         record[key] = (record[key]-self.stats[key]['mean'])/self.stats[key]['std']
