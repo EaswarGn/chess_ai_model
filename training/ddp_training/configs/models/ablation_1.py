e298c95c9b41df2f6adda8366dd8b32f740daddd
EaswarGn <easwar.chess@gmail.com>
2025-03-10 13:10:32 -0700

    updated code for final run

--- training/ddp_training/configs/models/ablation_1.py
+++ training/ddp_training/configs/models/ablation_1.py
@@ -81,7 +81,7 @@ class CONFIG:
         self.BOARD_STATUS_LENGTH = 70
         self.USE_AMP = True
         self.OPTIMIZER = torch.optim.Adam
-        self.USE_STRICT = False #use strict loading when loading a checkpoint?
+        self.USE_STRICT = True #use strict loading when loading a checkpoint?
         self.CHECKPOINT_PATH = '../../../1900_step_10000.pt'
         self.VALIDATION_STEPS = 100 #number of validation steps (each step has BATCH_SIZE samples)
 
