f8e12b20ed18df90a85fcf25291a2115c258d463
EaswarGn <easwar.chess@gmail.com>
2025-03-12 17:05:32 -0700

    testing training with batch norm

--- training/ddp_training/modules_ddp.py
+++ training/ddp_training/modules_ddp.py
@@ -688,7 +688,7 @@ class ExperimentalBoardEncoder(nn.Module):
         
         # Continuous Feature Projections (ensure output remains float)
         self.time_control_projection = nn.Sequential(
-            nn.Linear(2, d_model),  # Input size is 2 (initial time + increment time)
+            nn.Linear(2, d_model//2),  # Input size is 2 (initial time + increment time)
             nn.BatchNorm1d(d_model // 2),
             nn.ReLU(),
             nn.Linear(d_model, d_model),
