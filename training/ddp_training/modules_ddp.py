c4bfbdae6833ec2b41cd9259a505948315aeb7b6
EaswarGn <easwar.chess@gmail.com>
2025-03-12 17:18:48 -0700

    testing training with batch norm

--- training/ddp_training/modules_ddp.py
+++ training/ddp_training/modules_ddp.py
@@ -842,7 +842,7 @@ class ExperimentalBoardEncoder(nn.Module):
         """
         batch_size = turns.size(0)
         
-        x = torch.randn(512, 1)  # Example input tensor (batch_size=512, feature_size=1)
+        x = torch.randn(512, 1, device="cuda:0")  # Example input tensor (batch_size=512, feature_size=1)
 
         # Get the first linear layer
         first_linear = self.move_number_projection[0]  
