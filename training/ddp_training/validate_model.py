bd1222ac4c3e3302119881ba8f8a5be43286dc5c
EaswarGn <easwar.chess@gmail.com>
2025-03-14 13:35:17 -0700

    testing training with batch norm

--- training/ddp_training/validate_model.py
+++ training/ddp_training/validate_model.py
@@ -118,7 +118,7 @@ def validate_model(rank, world_size, CONFIG):
         prefetch_factor=CONFIG.PREFETCH_FACTOR,
     )
     
-    print(val_loader.dataset.datapoints_skipped)
+    
     
     model.eval()
     
@@ -167,6 +167,8 @@ def validate_model(rank, world_size, CONFIG):
                 moves_until_end_loss = loss_details['moves_until_end_loss']
                 categorical_game_result_loss = loss_details['categorical_game_result_loss']
 
+            print(val_loader.dataset.datapoints_skipped)
+            
             losses.update(
                 loss.item(), batch["lengths"].sum().item()
             )
