6e544e8239281c4b22a8a67ebb42983fa4f5e47c
EaswarGn <easwar.chess@gmail.com>
2025-03-15 20:46:05 -0700

    testing focal loss

--- training/ddp_training/configs/models/ablation_1.py
+++ training/ddp_training/configs/models/ablation_1.py
@@ -108,8 +108,9 @@ class CONFIG:
         }
 
         
+        weights = torch.tensor([1.0, 5.0, 1.0])
         self.move_loss = self.CRITERION
         self.move_time_loss = None #nn.L1Loss()
         self.moves_until_end_loss = None #nn.L1Loss()
-        self.categorical_game_result_loss = FocalLoss
+        self.categorical_game_result_loss = nn.CrossEntropyLoss(weight=weights, label_smoothing=0.2)
 
