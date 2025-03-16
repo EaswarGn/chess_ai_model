a7f806e005b109d463521dbe08d76c802673432f
EaswarGn <easwar.chess@gmail.com>
2025-03-15 20:53:02 -0700

    testing focal loss

--- training/ddp_training/criteria_ddp.py
+++ training/ddp_training/criteria_ddp.py
@@ -93,7 +93,7 @@ class MultiTaskChessLoss(nn.Module):
             'move_loss': CONFIG.move_loss(DEVICE=device, eps=CONFIG.LABEL_SMOOTHING, n_predictions=CONFIG.N_MOVES),
             'time_loss': CONFIG.move_time_loss,
             'moves_until_end_loss': CONFIG.moves_until_end_loss,
-            'categorical_game_result_loss': CONFIG.categorical_game_result_loss(weights=weights.to(device), label_smoothing=label_smoothing)
+            'categorical_game_result_loss': CONFIG.categorical_game_result_loss(weight=weights.to(device), label_smoothing=label_smoothing)
         }
 
     def forward(self, predictions, targets):
