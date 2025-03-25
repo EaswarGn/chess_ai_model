0507623d2e6ad8681a3ec875d1d07338e9e08a86
EaswarGn <easwar.chess@gmail.com>
2025-03-24 18:48:40 -0700

    testing experimental

--- training/ddp_training/configs/models/pondering_time_model.py
+++ training/ddp_training/configs/models/pondering_time_model.py
@@ -82,7 +82,7 @@ class CONFIG:
         self.USE_AMP = True
         self.OPTIMIZER = torch.optim.Adam
         self.USE_STRICT = True #use strict loading when loading a checkpoint?
-        self.CHECKPOINT_PATH = '../../../1900_step_15000.pt'#'../../../full_trained_model.pt'
+        self.CHECKPOINT_PATH = '../../../pondering_time_step_15000.pt'#'../../../full_trained_model.pt'
         self.VALIDATION_STEPS = 100 #number of validation steps (each step has BATCH_SIZE samples)
 
         ###############################
