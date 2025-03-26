b5e0e3a50df73bdc68b8d964c794ae9971e525de
EaswarGn <easwar.chess@gmail.com>
2025-03-25 19:38:16 -0700

    testing experimental

--- training/ddp_training/validate_model.py
+++ training/ddp_training/validate_model.py
@@ -84,8 +84,6 @@ def validate_model(rank, world_size, CONFIG):
             new_key = new_key.replace('module.', '')
             #new_key = 'module.'+new_key
             new_state_dict[new_key] = value
-            print(type(value))
-        model.load_state_dict(new_state_dict, strict=CONFIG.USE_STRICT)
             
         move_time_checkpoint = torch.load('../../../pondering_time_step_22000.pt', map_location=DEVICE, weights_only=False)
         state_dict = move_time_checkpoint['model_state_dict']
@@ -95,7 +93,6 @@ def validate_model(rank, world_size, CONFIG):
             new_key = new_key.replace('module.', '')
             #new_key = 'module.'+new_key
             move_time_model_state_dict[new_key] = value
-        print(new_state_dict['time_suggestion_cls_token'])
         new_state_dict['time_suggestion_cls_token'] = move_time_model_state_dict['time_suggestion_cls_token']
         new_state_dict['move_time_head.0.weight'] = move_time_model_state_dict['move_time_head.0.weight']
         new_state_dict = move_time_model_state_dict['move_time_head.0.weight']
