abecd9b976b18c1ebf7206c277ae5d2c74d746c3
EaswarGn <easwar.chess@gmail.com>
2025-03-30 08:55:02 -0700

    testing softmax sampling

--- harmonia-individual/validate_model.py
+++ harmonia-individual/validate_model.py
@@ -279,6 +279,30 @@ def validate_model(rank, world_size, CONFIG):
                         target_move = SQUARE_NAMES[sample['from_squares'][0].item()] + SQUARE_NAMES[sample['to_squares'][0].item()]
                         if target_move == move:
                             correct += 1
+                            
+                            
+                if rank==0:
+                    
+                    print(f"using range: {range_values[s]}")
+                    print(f"Move prediction accuracy: {correct/total}")
+                    print("Validation loss: %.3f" % losses.avg)
+                    #print("Validation move loss: %.3f" % move_losses.avg)
+                    print("Validation result loss: %.3f" % result_losses.avg)
+                    print("Validation move time loss: %.3f" % move_time_losses.avg)
+                    print("Validation moves until end loss: %.3f" % moves_until_end_losses.avg)
+                    print("Validation Categorical game result loss: %.3f" % categorical_game_result_losses.avg)
+                    """print("Validation Categorical game result accuracy: %.3f" % categorical_game_result_accuracies.avg)
+                    print("Validation top-1 accuracy: %.3f" % top1_accuracies.avg)
+                    print("Validation top-3 accuracy: %.3f" % top3_accuracies.avg)
+                    print("Validation top-5 accuracy: %.3f" % top5_accuracies.avg)"""
+                    #print(f"{datapoints_skipped} datapoints skipped from validation set.")
+                    
+                    s+=1
+                    pbar.close()
+                    pbar = tqdm(total=total_steps, desc="Validating")
+                    """pbar.close()
+                    cleanup_ddp()
+                    sys.exit()"""
                         
                                 
                 
@@ -286,7 +310,7 @@ def validate_model(rank, world_size, CONFIG):
                 top3_accuracies.update(top3_accuracy, batch["lengths"].shape[0])
                 top5_accuracies.update(top5_accuracy, batch["lengths"].shape[0])"""
                 
-                if predictions['categorical_game_result'] is None:
+                """if predictions['categorical_game_result'] is None:
                     categorical_game_result_accuracies.update(0.0, batch['lengths'].shape[0])
                 else:
                     categorical_game_result_accuracies.update(calculate_accuracy(predictions['categorical_game_result'].float(),
@@ -301,28 +325,9 @@ def validate_model(rank, world_size, CONFIG):
                     cleanup_ddp()
                     break
             
-            datapoints_skipped = (len(val_loader)-total_batches_processed)*CONFIG.BATCH_SIZE
+            datapoints_skipped = (len(val_loader)-total_batches_processed)*CONFIG.BATCH_SIZE"""
                 
-            if rank==0:
-                
-                print(f"using range: {range_values[s]}")
-                print(f"Move prediction accuracy: {correct/total}")
-                print("Validation loss: %.3f" % losses.avg)
-                #print("Validation move loss: %.3f" % move_losses.avg)
-                print("Validation result loss: %.3f" % result_losses.avg)
-                print("Validation move time loss: %.3f" % move_time_losses.avg)
-                print("Validation moves until end loss: %.3f" % moves_until_end_losses.avg)
-                print("Validation Categorical game result loss: %.3f" % categorical_game_result_losses.avg)
-                """print("Validation Categorical game result accuracy: %.3f" % categorical_game_result_accuracies.avg)
-                print("Validation top-1 accuracy: %.3f" % top1_accuracies.avg)
-                print("Validation top-3 accuracy: %.3f" % top3_accuracies.avg)
-                print("Validation top-5 accuracy: %.3f" % top5_accuracies.avg)"""
-                print(f"{datapoints_skipped} datapoints skipped from validation set.")
-                
-                s+=1
-                """pbar.close()
-                cleanup_ddp()
-                sys.exit()"""
+            
             
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
