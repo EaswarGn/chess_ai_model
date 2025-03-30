49621e32d7498e9917d4bcd3ae42b305e8560191
EaswarGn <easwar.chess@gmail.com>
2025-03-30 08:48:32 -0700

    testing softmax sampling

--- harmonia-individual/configs/models/individual_model.py
+++ harmonia-individual/configs/models/individual_model.py
@@ -87,7 +87,7 @@ class CONFIG:
         self.OPTIMIZER = torch.optim.Adam
         self.WEIGHT_DECAY = 0
         self.USE_STRICT = True #use strict loading when loading a checkpoint?
-        self.CHECKPOINT_PATH = '../../1900_step_3000.pt'
+        self.CHECKPOINT_PATH = '../../1900_step_30000.pt'
         self.VALIDATION_STEPS = 100 #number of validation steps (each step has BATCH_SIZE samples)
 
         ###############################
