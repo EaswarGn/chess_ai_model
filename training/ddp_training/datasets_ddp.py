176320dbb6c094c4a21e005ace7dbbd4e52badd6
EaswarGn <easwar.chess@gmail.com>
2025-04-14 16:53:48 -0700

    testing removing inputs

--- training/ddp_training/datasets_ddp.py
+++ training/ddp_training/datasets_ddp.py
@@ -138,6 +138,11 @@ class ChunkLoader(IterableDataset):
                     
                     if int(record['move_number']) <= 8:
                         record["moves_until_end"] = 35
+                        
+                    if int(record["turn"]) == 0:
+                        record["turn"] = 1
+                    else:
+                        record["turn"] = 0
 
     
                     try:
