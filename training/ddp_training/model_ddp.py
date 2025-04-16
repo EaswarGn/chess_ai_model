1448e3cbdb5742fa90d6b483a0543f7dbf143e46
EaswarGn <easwar.chess@gmail.com>
2025-04-15 19:25:51 -0700

    testing removing inputs

--- training/ddp_training/model_ddp.py
+++ training/ddp_training/model_ddp.py
@@ -143,7 +143,6 @@ class ChessTemporalTransformerEncoder(nn.Module):
         
         from_squares = (self.from_squares(boards[:, 11:, :]).squeeze(2).unsqueeze(1)) if self.from_squares is not None else None
         to_squares = (self.to_squares(boards[:, 11:, :]).squeeze(2).unsqueeze(1)) if self.to_squares is not None else None
-        print(from_squares.shape)
         moves_until_end = self.game_length_head(boards[:, 0:1, :]).squeeze(-1) if self.game_length_head is not None else None
         game_result = self.game_result_head(boards[:, 1:2, :]).squeeze(-1) if self.game_result_head is not None else None
         move_time = self.move_time_head(boards[:, 2:3, :]).squeeze(-1) if self.move_time_head is not None else None
