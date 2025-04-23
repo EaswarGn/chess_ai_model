0d95d898b204d96731a6c48117be1951859ecb39
EaswarGn <easwar.chess@gmail.com>
2025-04-22 19:39:11 -0700

    testing removing inputs

--- training/ddp_training/datasets_ddp.py
+++ training/ddp_training/datasets_ddp.py
@@ -177,16 +177,23 @@ class ChunkLoader(IterableDataset):
                     #normalizing through log transformations
                     #record['time_spent_on_move'] = np.log1p(record['time_spent_on_move'])
                     
-                    if record["turn"] == 1:
-                        record["black_remaining_time"] = 0.0
+                    """if record["turn"] == 0:  # White's turn
+                        record["white_remaining_time"] = abs(record["white_remaining_time"])  # Ensure it's positive
+                        record["black_remaining_time"] = -abs(record["black_remaining_time"])  # Make black's time negative
+                    else:  # Black's turn
+                        record["black_remaining_time"] = abs(record["black_remaining_time"])  # Ensure it's positive
+                        record["white_remaining_time"] = -abs(record["white_remaining_time"])  # Make white's time negative
+                    """
+                        
+                    
                     #record["white_remaining_time"] = 0.0
                     #record["black_remaining_time"] = 0.0
                     #record["phase"] = 0
                     #record["move_number"] = -1
                     #record["num_legal_moves"] = -1
-                    #record["white_material_value"] = -1
-                    #record["black_material_value"] = -1 
-                    #record["material_difference"] = -1
+                    record["white_material_value"] = -1
+                    record["black_material_value"] = -1 
+                    record["material_difference"] = -1
                     
                     yield {
                         "turn": torch.tensor([record["turn"]]).float(), #make float
