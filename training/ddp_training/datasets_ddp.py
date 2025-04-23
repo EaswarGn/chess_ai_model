b16f3a21fecebb2de622b086f2b3f62d779fa4b8
EaswarGn <easwar.chess@gmail.com>
2025-04-22 19:15:23 -0700

    testing removing inputs

--- training/ddp_training/datasets_ddp.py
+++ training/ddp_training/datasets_ddp.py
@@ -183,7 +183,7 @@ class ChunkLoader(IterableDataset):
                     #record["phase"] = 0
                     #record["move_number"] = -1
                     #record["num_legal_moves"] = -1
-                    #record["white_material_value"] = -1
+                    record["white_material_value"] = -1
                     record["black_material_value"] = -1 
                     #record["material_difference"] = -1
                     
