9cd14f11f3f63f063aa8cc4c0a49d7fe0ba54354
EaswarGn <easwar.chess@gmail.com>
2025-04-22 19:19:55 -0700

    testing removing inputs

--- training/ddp_training/datasets_ddp.py
+++ training/ddp_training/datasets_ddp.py
@@ -177,15 +177,14 @@ class ChunkLoader(IterableDataset):
                     #normalizing through log transformations
                     #record['time_spent_on_move'] = np.log1p(record['time_spent_on_move'])
                     
-                    #record["white_remaining_time"] = 0.0
+                    record["white_remaining_time"] = 0.0
                     #record["black_remaining_time"] = 0.0
-                    
                     #record["phase"] = 0
                     #record["move_number"] = -1
                     #record["num_legal_moves"] = -1
-                    record["white_material_value"] = -1
-                    record["black_material_value"] = -1 
-                    record["material_difference"] = -1
+                    #record["white_material_value"] = -1
+                    #record["black_material_value"] = -1 
+                    #record["material_difference"] = -1
                     
                     yield {
                         "turn": torch.tensor([record["turn"]]).float(), #make float
